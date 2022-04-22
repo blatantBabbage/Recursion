5eb62e6c564fd961803f57af942b890d5f7ab6af
blatantBabbage <ransinghbel@gmail.com>
2022-04-22 21:29:19 +0530

    pushing local ws

---
+++ src/com/algorithms/recursion/FactorialRecursion.java
@@ -0,0 +1,30 @@
+package com.algorithms.recursion;
+
+public class FactorialRecursion {
+
+    public static void main(String[] args) {
+        FactorialRecursion recursion = new FactorialRecursion();
+        /**
+         * The var keyword was introduced in Java 10. Type inference is used in var keyword in
+         * which it detects automatically the datatype of a variable based on the surrounding context.
+         * var cannot be used in an instance and global variable declaration.
+         * var cannot be used as a Generic type.
+         * var cannot be used for method parameters and return type.
+         */
+        var result = recursion.factorial(0);
+        System.out.println(result);
+
+    }
+
+    public int factorial(int n) {
+
+        if (n<1 && n != 0) {
+            return -1;
+        }
+
+        if (n == 0 || n == 1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+}
